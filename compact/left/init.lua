fd3b52e77ebd21597f3d4cac2b45cbcb08a2a36f
tupoll <tupollgbt@gmail.com>
2016-12-14 13:23:17 +0000

    Signed-off-by: tupoll <tupollgbt@gmail.com>

---
+++ compact/left/init.lua
@@ -0,0 +1,26 @@
+local awful     = require("awful")
+local beautiful = require("beautiful")
+local wibox     = require("wibox")
+local radical   = require("radical")
+local common    = require("compact.common")
+local l    = require("compact.layout")
+local m    = require("compact.menu")
+local t    = require("wibox.widget.systray")
+local a       = require("compact.avplay")
+
+local function new()
+    local layout = wibox.layout.flex.vertical()
+    local top_layout = wibox.layout.fixed.horizontal()
+    top_layout:add(l())   
+    top_layout:add(m())
+    
+    local bottom_layout = wibox.layout.fixed.horizontal()        
+    bottom_layout:add(t())
+    bottom_layout:add(a())
+    
+    layout:add(top_layout)
+    layout:add(bottom_layout)
+    return layout
+end
+
+return (function(_, ...) return new(...) end )
