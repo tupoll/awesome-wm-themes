e34c016f2318c72802715196bc1d28c7d58b8cf5
tupoll <tupollgbt@gmail.com>
2019-11-22 13:42:00 +0000

    Signed-off-by: tupoll <tupollgbt@gmail.com>

---
+++ compact/net/init.lua
@@ -0,0 +1,6 @@
+
+return {
+        net_dragonfly = require("compact.net.net_dragonfly"),
+        net      = require("compact.net.net_vicious")        
+        
+  }
---
+++ compact/net/net_vicious.lua
@@ -0,0 +1,28 @@
+local awful     = require("awful")
+local wibox     = require("wibox")
+local beautiful = require("beautiful")
+local radical   = require("radical")
+local common    = require("compact.common.helpers1")
+local vicious = require("vicious")
+
+local module = {}
+
+netdowninfo = wibox.widget.textbox()
+vicious.register(netdowninfo, vicious.widgets.net,"${eth0 down_kb}", 1 )
+local netupinfo = wibox.widget.textbox()
+vicious.register(netupinfo, vicious.widgets.net,"${eth0 up_kb}", 2 )
+
+-- Return widgets layout
+local function new()
+    local layout = wibox.layout.fixed.horizontal()
+    local widget_txt1,text = common.textbox({ text=" ", width=30 })    
+    local widget_img1,img = common.imagebox({ icon=beautiful.path.."/widgets/up.png" })         
+    local widget_img2,img = common.imagebox({ icon=beautiful.path.."/widgets/kbs.png" })    
+    local widget_img3,img = common.imagebox({ icon=beautiful.path.."/widgets/down.png" })    
+    local widget_img4,img = common.imagebox({ icon=beautiful.path.."/widgets/kbs.png" })
+    local widget_txt2,text = common.textbox({ text="", width=5})
+    layout:add(widget_txt1 ,widget_img1, netupinfo, widget_img2, widget_txt2, widget_img3, netdowninfo, widget_img4, widget_txt1)   
+    return layout
+end
+
+return setmetatable(module, { __call = function(_, ...) return new(...) end })
