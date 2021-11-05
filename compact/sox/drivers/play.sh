5dbc3852e76c058250463183e5cb5ee7945a4021
tupoll <tupollgbt@gmail.com>
2021-11-05 08:29:52 +0000

    demon to play

---
+++ compact/sox/drivers/play.sh
@@ -0,0 +1,35 @@
+#!/usr/local/bin/zsh
+
+mkdir -p ~/tmp/sox/players/
+DIR2=~/tmp/sox/players
+cp -R ~/tmp/playlist ~/tmp/sox/playlist
+cat ~/tmp/sox/playlist |sed '1d'|  /usr/bin/awk '{print $0}'>~/tmp/playlist
+head -n 1 ~/tmp/sox/playlist | tail -n1 >~/tmp/sox/name
+cat ~/tmp/sox/name>>~/tmp/playlist1
+A=$(cat ~/tmp/sox/name |  /usr/bin/awk '{print $0}')
+cp $A $DIR2
+
+~/.config/awesome/compact/sox/drivers/start.lua  
+rm -rf $DIR2
+rm -rf ~/tmp/sox/name
+cat ~/tmp/sox/playlist |sed '1,2d'|  /usr/bin/awk '{print $0}'>~/tmp/playlist
+head -n 2 ~/tmp/sox/playlist | tail -n1 >~/tmp/sox/name
+cat ~/tmp/sox/name>>~/tmp/playlist1
+B=$(cat ~/tmp/sox/name |  /usr/bin/awk '{print $0}')
+mkdir -p $DIR2
+cp $B $DIR2
+
+~/.config/awesome/compact/sox/drivers/start.lua
+rm -rf $DIR2
+rm -rf ~/tmp/sox/name
+cat ~/tmp/sox/playlist |sed '1,3d'|  /usr/bin/awk '{print $0}'>~/tmp/playlist
+head -n 3 ~/tmp/sox/playlist | tail -n1 >~/tmp/sox/name
+cat ~/tmp/sox/name>>~/tmp/playlist1
+C=$(cat ~/tmp/sox/name |  /usr/bin/awk '{print $0}')
+mkdir -p $DIR2
+cp  $C $DIR2
+
+~/.config/awesome/compact/sox/drivers/start.lua
+rm -rf $DIR2
+rm -rf ~/tmp/sox/name
+~/.config/awesome/compact/sox/drivers/play0.sh
