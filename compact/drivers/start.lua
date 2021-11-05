1e21063eb4ce880b1ea0e24e2425590174a80128
tupoll <tupollgbt@gmail.com>
2021-11-05 09:50:22 +0000

    Delete compact/drivers directory

--- compact/drivers/start.lua
+++
@@ -1,5 +0,0 @@
-#!/usr/local/bin/lua52
-function start() os.execute("play --vol 0.08 /home/$USER/tmp/sox/players/*.* rate -v 48000 bass +10 treble +15") end
-function stop() os.execute("pkill -f play") end
-start()
-stop()
