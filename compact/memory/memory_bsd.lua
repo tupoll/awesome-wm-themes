85ed9f1a97bbddfbc7e1efe0e7a4703580cc64cb
tupoll <tupollgbt@gmail.com>
2021-11-05 10:24:41 +0000

    Update memory_bsd.lua

--- compact/memory/memory_bsd.lua
+++ compact/memory/memory_bsd.lua
@@ -2,12 +2,27 @@ local awful     = require("awful")
 local wibox     = require("wibox")
 local beautiful = require("beautiful")
 local radical   = require("radical")
-local common    = require("compact.common.helpers1")
 local vicious = require("vicious")
 local blingbling = require("blingbling")
 local naughty       = require('naughty')
 
 local module = {}
+
+ local function keymap(...)
+   local t = {}
+   for _, k in ipairs({...}) do
+      local but
+      if type(k[1]) == "table" then
+         but = awful.button(k[1], k[2], k[3])
+      else
+         but = awful.button({}, k[1], k[2])
+      end
+      t = awful.util.table.join(t, but)
+   end
+   return t
+end
+  local mouse = { LEFT = 1, MIDDLE = 2, RIGHT = 3, WHEEL_UP = 4, WHEEL_DOWN = 5 }
+
 
 mem_graph = blingbling.line_graph({ height = 29,
                                       width = 60,
@@ -34,8 +49,13 @@ function memory_status()
         end
     )
 end
-mem_graph:connect_signal("mouse::enter", function() memory_status() end)
-mem_graph:connect_signal("mouse::leave", function() naughty.destroy(notification) end)
+--mem_graph:connect_signal("mouse::enter", function() memory_status() end)
+--mem_graph:connect_signal("mouse::leave", function() naughty.destroy(notification) end)
+
+mem_graph:buttons(
+      keymap({ mouse.LEFT,function() memory_status() end},
+      { mouse.LEFT, function()naughty.destroy(n)end }))
+
 -- Return widgets layout
 local function new()
     local layout = wibox.layout.fixed.horizontal()
