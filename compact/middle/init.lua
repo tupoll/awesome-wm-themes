e8c29d4596fe961d9165cebd9fb0864310baab00
tupoll <tupollgbt@gmail.com>
2021-11-05 10:20:38 +0000

    Update init.lua

--- compact/middle/init.lua
+++ compact/middle/init.lua
@@ -2,22 +2,26 @@ local awful     = require("awful")
 local beautiful = require("beautiful")
 local wibox     = require("wibox")
 local radical   = require("radical")
-local l    = require("compact.layout")
-local a       = require("compact.sox")
+local u    = require("compact.unitybar")
 local p    = require("compact.places")
 
 local function new()
-    local layout = wibox.layout.flex.vertical()
-    local top_layout = wibox.layout.fixed.horizontal()
-    top_layout:add(l())   
+    local layout = wibox.layout.fixed.horizontal()
+    local right_layout = wibox.layout.fixed.horizontal()
+    local left_layout = wibox.layout.flex.vertical()
+   
+    right_layout:add(u())
+          
+    local top_layout = wibox.layout.fixed.horizontal()    
     top_layout:add(p())
+            
+    left_layout:add(top_layout)
+    local bottom_layout = wibox.layout.fixed.horizontal()
     
-    local bottom_layout = wibox.layout.fixed.horizontal()        
-  
-    bottom_layout:add(a())
-    
-    layout:add(top_layout)
-    layout:add(bottom_layout)
+        
+    left_layout:add(bottom_layout)
+    layout:add(left_layout)
+    layout:add(right_layout)
     return layout
 end
 
