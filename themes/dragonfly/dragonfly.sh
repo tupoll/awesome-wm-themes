566353e2468d5d213063ce2c98b15f492e5fcdb2
tupoll <tupollgbt@gmail.com>
2021-12-06 10:27:51 +0000

    Create dragonfly.sh

---
+++ themes/dragonfly/dragonfly.sh
@@ -0,0 +1,11 @@
+#!/usr/local/bin/zsh
+ln -sf   ~/.config/awesome/themes/pattern/play  ~/.config/awesome/themes/dragonfly/play
+ln -sf   ~/.config/awesome/themes/pattern/icons  ~/.config/awesome/themes/dragonfly/icons
+ln -sf   ~/.config/awesome/themes/pattern/launcher  ~/.config/awesome/themes/dragonfly/launcher
+ln -sf   ~/.config/awesome/themes/pattern/logos  ~/.config/awesome/themes/dragonfly/logos
+ln -sf   ~/.config/awesome/themes/pattern/layouts  ~/.config/awesome/themes/dragonfly/layouts
+ln -sf   ~/.config/awesome/themes/pattern/mixer  ~/.config/awesome/themes/dragonfly/mixer
+ln -sf   ~/.config/awesome/themes/pattern/naughty  ~/.config/awesome/themes/dragonfly/naughty
+ln -sf   ~/.config/awesome/themes/pattern/places  ~/.config/awesome/themes/dragonfly/places
+ln -sf   ~/.config/awesome/themes/pattern/taglist  ~/.config/awesome/themes/dragonfly/taglist
+ln -sf   ~/.config/awesome/themes/pattern/widgets  ~/.config/awesome/themes/dragonfly/widgets
